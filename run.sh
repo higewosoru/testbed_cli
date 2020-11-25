f7c8ef5764f3bbdc818d9588b200e281e7ba95ba
BSI_tech <christien@bsi-cr.com>
2020-11-25 12:27:20 -0800

    installed on CentOS

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #run the application with the python virtual environment interpreter.
 
-source env/bin/activate
-pip3 install -r requirements.txt
+source ./env/bin/activate
+#pip3 install -r requirements.txt
 
 cd app/
 python3 app.py
